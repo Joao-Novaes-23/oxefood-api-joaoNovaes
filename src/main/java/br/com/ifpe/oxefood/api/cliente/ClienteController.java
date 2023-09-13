239a0c1a5b883ccac60ba30525957550e5dd6f89
aluno <aluno@ifpe>
2023-09-13 11:20:57 -0300

    commit

---
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -0,0 +1,46 @@
+package br.com.ifpe.oxefood.api.cliente;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+
+@RestController
+@RequestMapping("/api/cliente")
+@CrossOrigin
+public class ClienteController {
+
+    @GetMapping
+    public List<Cliente> findAll() {
+  
+        return clienteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable Long id) {
+
+        return clienteService.findById(id);
+    }
+
+   @Autowired
+   private ClienteService clienteService;
+
+   @PostMapping
+   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+
+       Cliente cliente = clienteService.save(request.build());
+       return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+   }
+}
+
+
---
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -0,0 +1,37 @@
+package br.com.ifpe.oxefood.modelo.cliente;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    public List<Cliente> findAll() {
+  
+        return repository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+   @Autowired
+   private ClienteRepository repository;
+
+   @Transactional
+   public Cliente save(Cliente cliente) {
+
+       cliente.setHabilitado(Boolean.TRUE);
+       cliente.setVersao(1L);
+       cliente.setDataCriacao(LocalDate.now());
+       return repository.save(cliente);
+   }
+
+}
+
