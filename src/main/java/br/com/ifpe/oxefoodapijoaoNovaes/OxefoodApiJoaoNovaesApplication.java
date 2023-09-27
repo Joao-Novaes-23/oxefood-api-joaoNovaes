f8841bbd9fa18473ab1273da556084690c340869
João Victor Peregrino Novaes <jvpn@discente.ifpe.edu.br>
2023-09-26 21:04:24 -0300

    pro repositorio

---
+++ src/main/java/br/com/ifpe/oxefoodapijoaoNovaes/OxefoodApiJoaoNovaesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapijoaoNovaes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiJoaoNovaesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiJoaoNovaesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodapijoaoNovaes/OxefoodApiJoaoNovaesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapijoaoNovaes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiJoaoNovaesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
